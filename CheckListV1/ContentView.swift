00daf471b8fa65ba9153113d5b3ce56644b34f5a
In-Hye Kye <sadnody@gmail.com>
2021-05-25 00:43:16 +0900

    use Looping

--- CheckListV1/ContentView.swift
+++ CheckListV1/ContentView.swift
@@ -8,35 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var checklistItems = ["Walk the dog", "Brush my teeth", "Learn iOS development", "Soccer practice", "Eat ice cream"]
+    
     var body: some View {
         NavigationView{
             List {
-                Section(header: Text("High priority")){
-                    Group{
-                        Text("Walk the dog")
-                        Text("Brush my teeth")
-                        Text("Learn iOS development")
-                        Text("Make dinner")
-                        Text("Do laundry")
-                        Text("Pay bills")
-                    }
-                    Group{
-                        Text("Finish homework")
-                        Text("Change internet provider")
-                        Text("Clean the kitchen")
-                        Text("Wash the car")
-                        Text("Wash the hair")                        
-                    }
-                }
-                Section(header: Text("Low priority")){
-                    Text("Soccer practice")
-                    Text("Eat Ice cream")
+                ForEach(checklistItems, id: \.self){
+                    item in Text(item)
+                        .onTapGesture {
+                            self.checklistItems.append(item)
+                            self.printChecklistContents()
+                        }
                 }
             } // End of list
-            .listStyle(GroupedListStyle())
             .navigationBarTitle("Checklist")
+            .onAppear(){
+                self.printChecklistContents()
+            }
         }// End of navigationView
     } // End of body
+    
+    //Method
+    func printChecklistContents(){
+        for item in checklistItems {
+            print(item)
+        }
+    }
 } // End of ContentView
 
 struct ContentView_Previews: PreviewProvider {
