806f73b69e66c52888bd8a0b504554e3486c20aa
In-Hye Kye <sadnody@gmail.com>
2021-05-24 23:42:10 +0900

    Initial Commit

---
+++ CheckListV1/CheckListV1App.swift
@@ -0,0 +1,17 @@
+//
+//  CheckListV1App.swift
+//  CheckListV1
+//
+//  Created by RelMac User Exercise2 on 2021/05/24.
+//
+
+import SwiftUI
+
+@main
+struct CheckListV1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
