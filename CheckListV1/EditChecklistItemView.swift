1b8ae7377d437ad86f7cdb8088e4d091fd5ef518
In-Hye Kye <sadnody@gmail.com>
2021-06-04 01:10:28 +0900

    Change the starting scene

---
+++ CheckListV1/EditChecklistItemView.swift
@@ -0,0 +1,20 @@
+//
+//  EditChecklistItemView.swift
+//  CheckListV1
+//
+//  Created by RelMac User Exercise2 on 2021/06/04.
+//
+
+import SwiftUI
+
+struct EditChecklistItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditChecklistItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditChecklistItemView()
+    }
+}
